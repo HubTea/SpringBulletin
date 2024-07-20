6b147c02dd9ae6480a91b6e9ed834634e4e06fe5
HubTea <k3392255@naver.com>
2024-07-21 01:38:47 +0900

    테스트 코드 추가
    애플리케이션의 전반적인 기능을 테스트하는 테스트 케이스를 하나 추가했다.

--- src/test/java/com/bulletin/bulletin/BulletinApplicationTests.java
+++ src/test/java/com/bulletin/bulletin/BulletinApplicationTests.java
@@ -1,13 +1,44 @@
 package com.bulletin.bulletin;
 
+import com.bulletin.bulletin.service.ArticleService;
+import com.bulletin.bulletin.service.CommentService;
+import com.bulletin.bulletin.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BulletinApplicationTests {
 
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	ArticleService articleService;
+
+	@Autowired
+	CommentService commentService;
+
 	@Test
-	void contextLoads() {
+	void test() throws Exception{
+		String userId = "spring";
+		String[][] articleList = {
+				{"title1", "body1"},
+				{"title2", "body2"}
+		};
+		String[] commentList = {
+				"comment1", "comment2"
+		};
+
+		userService.create(userId);
+
+		for(String[] article: articleList) {
+			articleService.create(userId, article[0], article[1]);
+		}
+
+		for(String comment: commentList) {
+			commentService.create(userId, comment);
+		}
 	}
 
 }
